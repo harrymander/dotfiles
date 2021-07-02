a17b1cae075b3320616ae563abab9e0d783ed789
Harry Mander <harrymander96@gmail.com>
2021-07-02 15:18:46 +1200

    Fix shellcheck errors and ask before overwriting existing configs
    TODO: try to merge existing config?

--- git/setup.sh
+++ git/setup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-read -p 'Enter default git user name: ' username
-read -p 'Enter default git email address: ' email
+if [ -f "$HOME/.gitconfig" ]; then
+	read -rp ".gitconfig already exists, overwrite? [Y/n] " yn
+	case $yn in
+		[Yy]*) ;;
+		*) echo "Aborted."; exit;;
+	esac
+fi
 
-cat > $HOME/.gitconfig << EOF
+read -rp 'Enter default git user name: ' username
+read -rp 'Enter default git email address: ' email
+
+cat > "$HOME/.gitconfig" << EOF
 [user]
 	name = ${username}
 	email = ${email}
 [include]
-    path = $(dirname -- $(realpath -- ${BASH_SOURCE[0]}))/gitconfig
+    path = $(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")/gitconfig
 EOF
--- vim/setup.sh
+++ vim/setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
-curl -fLo $HOME/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+if [ -d "$HOME/.vim" ] || [ -f "$HOME/.vimrc" ]; then
+    read -rp ".vim directory and/or .vimrc exist, overwrite? [Y/n]" yn
+    case $yn in
+        [Yy]*) ;;
+        *) echo "Aborted."; exit;;
+    esac
+fi
 
-ln -sf $(dirname -- $(realpath -- ${BASH_SOURCE[0]}))/vimrc $HOME/.vim
-echo 'runtime vimrc' > $HOME/.vimrc
+mkdir -p "$HOME/.vim/autoload" "$HOME/.vim/bundle"
+curl -fLo "$HOME/.vim/autoload/plug.vim" "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+curl -fLo "$HOME/.vim/autoload/pathogen.vim" "https://tpo.pe/pathogen.vim"
+
+ln -sf "$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")/vimrc" "$HOME/.vim"
+echo 'runtime vimrc' > "$HOME/.vimrc"
 
 vim +PlugInstall +qall
