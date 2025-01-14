363ba77e738c855bcbcd40f13e5b7d171d042eed
Harry Mander <harry.mander@pg.canterbury.ac.nz>
2025-01-14 14:55:41 +1300

    vim: remove Pathogen, tidy up package installation

--- vim/setup.sh
+++ vim/setup.sh
@@ -8,9 +8,8 @@ if [ -d "$HOME/.vim" ] || [ -f "$HOME/.vimrc" ]; then
     esac
 fi
 
-mkdir -p "$HOME/.vim/autoload" "$HOME/.vim/bundle"
-curl -fLo "$HOME/.vim/autoload/plug.vim" "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-curl -fLo "$HOME/.vim/autoload/pathogen.vim" "https://tpo.pe/pathogen.vim"
+curl -fL --create-dirs -o "$HOME/.vim/autoload/plug.vim" \
+    "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 ln -sf "$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")/vimrc" "$HOME/.vim"
 > "$HOME/.vimrc" cat << EOF
