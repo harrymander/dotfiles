d18b531fb62856a07a09da9393ecc4a8ccac2350
Harry Mander <harrymander96@gmail.com>
2026-02-04 23:40:27 +1300

    bash: prompt: treat PROMPT_COMMAND as array

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -151,6 +151,6 @@ function _update_ps1() {
     PS1+="$NOCOL "
 }
 
-if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
-    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+if [[ ${PROMPT_COMMAND[*]:-} != *'_update_ps1'* ]]; then
+    PROMPT_COMMAND=(_update_ps1 "${PROMPT_COMMAND[@]}")
 fi
