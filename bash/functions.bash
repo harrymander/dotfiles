5ee2acf724a6f647031817e194c822fd2ea1467f
Harry Mander <harrymander96@gmail.com>
2026-06-10 23:46:51 +1200

    bash: functions: avoid syncing or updating uv lock file when looking or venv

--- bash/functions.bash
+++ bash/functions.bash
@@ -58,7 +58,7 @@ EOF
         _find_managed_venv() {
             >&2 echo "Not a virtualenv directory: $venv_dir; searching for venv with uv..."
             local uv_venv
-            if uv_venv=$(uv run python -c 'import sys; print(sys.prefix) if sys.base_prefix != sys.prefix else sys.exit(1)'); then
+            if uv_venv=$(uv run --no-sync --frozen python -c 'import sys; print(sys.prefix) if sys.base_prefix != sys.prefix else sys.exit(1)'); then
                 activate="source '$uv_venv/bin/activate'"
                 return 0
             fi
